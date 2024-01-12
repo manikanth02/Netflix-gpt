fe896c563a5e3514826f96fb7c8d74c155b4b2c6
manikanth02 <manikanth1602@gmail.com>
2024-01-12 16:52:22 +0530

    [Features] Add user name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,11 +50,13 @@ const Header = () => {
 
 
     return (
-        <div className="absolute w-screen px-8 py-2  z-10 flex flex-col md:flex-row justify-between">
-            <img className="w-44"  src={NETFLIX_LOGO} alt="NETFLIX LOGO"/>
-            {showGptSearch && (
+      <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={NETFLIX_LOGO} alt="logo" />
+      {user && (
+        <div className="flex p-2 justify-between">
+          {showGptSearch && (
             <select
-              className="p-2 m-2 bg-gray-900 text-white rounded"
+              className="p-2 m-2 bg-gray-900 text-white rounded-lg"
               onChange={handleLanguageChange}
             >
               {SUPPORTED_LANGUAGES.map((lang) => (
@@ -64,19 +66,30 @@ const Header = () => {
               ))}
             </select>
           )}
-            <button
-            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded"
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
             {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
-
-       { user && 
-        <div className="bg-red-700 p-[10px] rounded text-lg" >
-            <button onClick={() => handleSignOut()}>Sign Out</button>
+          {/* <img
+            className="hidden md:block w-12 h-12"
+            alt="usericon"
+            src={user?.photoURL}
+          /> */}
+          <div>
+          <button
+          className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+          >
+           { user?.displayName || "Manikanth"}
+          </button>
+          </div>
+          <button onClick={handleSignOut} className="p-2 m-2 bg-gray-900 text-white rounded-lg">
+            Sign Out
+          </button>
         </div>
-        }
-        </div>
+      )}
+    </div>
     )
 }
 

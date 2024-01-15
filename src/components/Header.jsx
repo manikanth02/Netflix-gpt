c3f9b05c8310dfec02ea703ed9d41ac44eca31d6
manikanth02 <manikanth1602@gmail.com>
2024-01-15 14:52:19 +0530

    [Features] add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -73,13 +73,16 @@ const Header = () => {
           >
             {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
-          <div>
           <button
           className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
           >
            { user?.displayName || "Manikanth"}
           </button>
-          </div>
+          <img
+            className="hidden md:block w-12 h-12"
+            alt="usericon"
+            src={user?.photoURL}
+          />
           <button onClick={handleSignOut} className="p-2 m-2 bg-gray-900 text-white rounded-lg">
             Sign Out
           </button>
